1088e070c2faca355d19c222be818cbd30c0cc71
novirael <novirael@gmail.com>
2016-11-27 06:21:49 +0100

    [python] queen backtracking multiple solutions

--- heuristics/queens/backtracking.py
+++ heuristics/queens/backtracking.py
@@ -1,16 +1,19 @@
+import time
+import copy
 
 class QueenBacktracking(object):
     board = None
-    solutions = None
+    solutions = []
     deep = 0
+    start_time = None
 
     def __init__(self, n):
         self.board = [[0 for _ in range(n)] for _ in range(n)]
 
     def resolve(self):
-
-        if self._try_to_put_queen(0):
-            self._preview_board()
+        self.start_time = time.time()
+        self._try_to_put_queen(0)
+        self._preview_board()
 
     def _try_to_put_queen(self, row):
         self.deep += 1
@@ -18,17 +21,19 @@ class QueenBacktracking(object):
         max_size = len(self.board)
         print(row >= max_size)
         if row >= max_size:
-            return True
-
-        for column in range(max_size):
-            if self._can_put_queen(row, column):
-
-                self.board[row][column] = 1
-                if self._try_to_put_queen(row + 1):
-                    return True
-                self.board[row][column] = 0
-
-        return False
+            self.solutions.append({
+                "board": copy.deepcopy(self.board),
+                "max_deep": self.deep,
+                "duration": time.time() - self.start_time
+            })
+            self.deep = 0
+            self.start_time = time.time()
+        else:
+            for column in range(max_size):
+                if self._can_put_queen(row, column):
+                    self.board[row][column] = 1
+                    self._try_to_put_queen(row + 1)
+                    self.board[row][column] = 0
 
     def _can_put_queen(self, row, column):
         max_size = len(self.board)
@@ -45,9 +50,11 @@ class QueenBacktracking(object):
         return True
 
     def _preview_board(self):
-        for row in self.board:
-            for column in row:
-                print(str(column) + ' ', end="")
+        for solution in self.solutions:
+            for row in solution['board']:
+                for column in row:
+                    print(str(column) + ' ', end="")
+                print()
             print()
 
 
